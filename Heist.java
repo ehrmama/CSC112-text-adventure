6eee20433cb2b193f8cfa8016b79270a9a09287e
ehrmama <ehrmama@whs.school.nz>
2025-06-04 09:19:05 +1200

    Test (#7)
    * bluejchanges
    
    * Update package.bluej
    
    * small changes
    
    * fixed stop and reset
    
    fixed stop and reset
    
    * bug fixing 00

--- Heist.java
+++ Heist.java
@@ -32,33 +32,33 @@ public class Heist
         "You slide into the ventilation shaft, snug as can be, you crawl along it until it reaches a \n fork, you think your odds are around 50/50 that you’ll end up in the diamond room.", 
         "You start your trek down the hallway but end up getting extremely lost until you reach an \n unlabeled room, thinking to yourself that this surely has to be something you enter, as you \n walk into the room you accidentally bump something, but in the dark you don’t think too much \n of it. That moment a large T-Rex skeleton starts falling straight towards you, its head coming \n down to trap you. Immediately two guards run into the room being alerted by the noise and \n arrest you.",
         "You Stare for a long time at the ventilation shaft contemplating, but in the end you decide \n to just steal the sleeping entrance guards wallet and call it a day. ( Petty theft ending )", 
-        "You walk into the room labeled staff only, in it you find a guards uniform that you decide to put on, as you finish putting on the uniform another guard walks into the room and says ‘you must be the new guy let me show you the ropes’ (Double agent ending )",
-        "You enter the Rare Minerals exhibit and find yourself staring at a block of uraninite for far to long in that moment you realise that you are extremely susceptible to radiation poisoning and stumble run out of the museum to the nearest hospital as fast as you can ( I tried so hard and got so far but in the end it didn’t even matter ending)",
-        "You take a left thinking that going left has never let you down before, as you are thinking that the vent collapses, dropping you right into the middle of a crowded guards room who instantly arrest you.",
-        "You take a right sticking to your pattern you take a right and before you know it you end up in the middle of the diamond room, now you sit directly above the floating case that holds the diamond since you don't have an item that will help you otherwise you think you might be able to climb down and open the case that way (L).",    
-        "As you ascend the flimsy rope you brought to the diamond you think that you might not reach it. As you think that you reach the top. Relieved, you haul up your rope and attempt to open the case. At that moment an alarm goes off, guards run into the room and turrets descend from the ceiling. You get arrested.",
-        "You use the sonic blaster, unbeknownst to you, you had it facing round the wrong way, the guard standing behind you gets evaporated and a large exit hole is created you  could leave now (R ) or you can attempt to open the diamond case another way.",
-        "You use the glass cutter extremely effectively, plucking the diamond out of the case and sneaking away through the ventilation shaft. (Henry Stickman Robbery ending )",
-        "You clumsy attempt to climb down to the case as you reach the top of the floating case turrets descend from the ceiling and alarms go off. An armada of guards storm into the room and arrest you.",
-        "You attempt to open the glass case with the keycard you find on the smouldering guards outfit to your surprise it opens. At that moment a patrol of 4 guards enter the room but don’t see you yet to your right is a big hole you’ve created and to your left is the room with the manhole from which you emerged.",
-        "You decide to leave knowing that you were responsible for the murder of an innocent man you flee into the night never to be seen again (Audiological assassin ending)",
-        "As you sprint towards the large hole in the wall that you’ve created the guards spot you. One pulls out his weapon and shoots you three times in the legs, you fall clutching your legs and are promptly arrested.",
-        "You Sprint toward the sewer grate that you luckily forgot to close. close the grate above you and run like the wind, diamond under your arm.  (Slimy sewer stealer ending)."
+        "You walk into the room labeled staff only, in it you find a guards uniform that you decide to \n put on, as you finish putting on the uniform another guard walks into the room and says \n‘you must be the new guy let me show you the ropes’ (Double agent ending )",
+        "You enter the Rare Minerals exhibit and find yourself staring at a block of uraninite for far to \n long in that moment you realise that you are extremely susceptible to radiation poisoning \n and stumble run out of the museum to the nearest hospital as fast as you can ( I tried so hard and got so far but in the end it didn’t even matter ending)",
+        "You take a left thinking that going left has never let you down before, as you are thinking that \n the vent collapses, dropping you right into the middle of a crowded guards room who \n instantly arrest you.",
+        "You take a right sticking to your pattern you take a right and before you know it you end up in \n the middle of the diamond room, now you sit directly above the floating case that holds \n the diamond since you don't have an item that will help you otherwise you think you might be able to climb down and open the case that way (L).",    
+        "As you ascend the flimsy rope you brought to the diamond you think that you might not reach it. \n As you think that you reach the top. Relieved, you haul up your rope and attempt to open \n the case. At that moment an alarm goes off, guards run into the room and turrets descend from the ceiling. You get arrested.",
+        "You use the sonic blaster, unbeknownst to you, you had it facing round the wrong way, the guard \n standing behind you gets evaporated and a large exit hole is created you  could leave \n now (R ) or you can attempt to open the diamond case another way.",
+        "You use the glass cutter extremely effectively, plucking the diamond out of the case and sneaking \n away through the ventilation shaft. (Henry Stickman Robbery ending )",
+        "You clumsy attempt to climb down to the case as you reach the top of the floating case turrets \n descend from the ceiling and alarms go off. An armada of guards storm into the room \n and arrest you.",
+        "You attempt to open the glass case with the keycard you find on the smouldering guards outfit to \n your surprise it opens. At that moment a patrol of 4 guards enter the room but \n don’t see you yet to your right is a big hole you’ve created and to your left is the room with the manhole from which you emerged.",
+        "You decide to leave knowing that you were responsible for the murder of an innocent man you flee \n into the night never to be seen again (Audiological assassin ending)",
+        "As you sprint towards the large hole in the wall that you’ve created the guards spot you. One pulls \n out his weapon and shoots you three times in the legs, you fall clutching your \n legs and are promptly arrested.",
+        "You Sprint toward the sewer grate that you luckily forgot to close. close the grate above you and \n run like the wind, diamond under your arm.  (Slimy sewer stealer ending)."
     };
     String[] view =  {"0", "0R", "0L","0RR", "0LL", "0LR", "0RRR", "0LLL", "0RRRR","0RRRRL"};
-    String [] viewID={"You see a nice ornate building towering in front of you. To your left you see a very \n grandiose entrance with pillars and everything, to your right you see a very dirty looking sewer \n entrance that you can smell from where you're standing."
-    ,"You are standing in a very dirty and smelly sewer. to your left you see an incredibly deep shaft for transferring waste water from the surface sewer to a deeper part of the sewer you can’t even make out the bottom, to your right is a very small shaft maybe a little wider than your torso looking ahead there is a huge river of waste that is wide enough you are sure you cannot cross it without getting caught in the current. "
-    , "Now that you’re a little closer to the entrance you see 2 guards standing at the entrance not really paying attention through the door you see a security counter with a slumbering guard in the chair. "
-    , "The room buzzes with the sound of a generator. To your left, the machine hums loudly. To your right, light leaks from a heavy door to the diamond room. Resting on a nearby shelf is a sleek, glowing weapon — a sonic blaster."
-    ,"The guard sitting behind the front desk is still asleep and nobody seems to suspect you due /n  to your boldness, to your left is a hallway , to your right a broken ventilation shaft sits. "
-    , "You’re hidden under a cluttered desk. Papers, keys, and an empty coffee cup litter the /n surface. To the left is a hallway; to the right, a broken vent beckons."
-    , "You stand under a huge floating ornate glass cabinet that holds the huge diamond /n  you've been looking for, above it you see a mess of cables holding it up."
+    String [] viewID={"You see a nice ornate building towering in front of you. To your left you see a very \n grandiose entrance with pillars and everything, to your right you see a very dirty \n looking sewer \n entrance that you can smell from where you're standing."
+    ,"You are standing in a very dirty and smelly sewer. to your left you see an incredibly deep shaft for \n transferring waste water from the surface sewer to a deeper part of the sewer you can’t \n even make out the bottom, to your right is a very small shaft maybe a little wider than your torso looking ahead there is a huge river of waste that is wide enough you are sure you cannot cross it without getting caught in the current. "
+    , "Now that you’re a little closer to the entrance you see 2 guards standing at the entrance not really \n  paying attention through the door you see a security counter with a slumbering guard in \n the chair. "
+    , "The room buzzes with the sound of a generator. To your left, the machine hums loudly. To your \n right, light leaks from a heavy door to the diamond room. Resting on a nearby shelf is a sleek, \n glowing weapon — a sonic blaster."
+    ,"The guard sitting behind the front desk is still asleep and nobody seems to suspect you due \n  to your boldness, to your left is a hallway , to your right a broken ventilation shaft sits. "
+    , "You’re hidden under a cluttered desk. Papers, keys, and an empty coffee cup litter the \n surface. To the left is a hallway; to the right, a broken vent beckons."
+    , "You stand under a huge floating ornate glass cabinet that holds the huge diamond \n  you've been looking for, above it you see a mess of cables holding it up."
     ,"You see two doors in front of you one is labeled rare minerals exhibit whereas the other \n is labeled ‘Staff Only’. You notice to your left is a shiny tool sitting on the ground that you might \n be able to use." 
     ,"There is now  a huge smoldering exit hole with some red mist floating around."
     ,"There's a huge hole and a door. You don’t see much since adrenalin."};
     String [] endingsID = {"0RRRRLL","0RRRRR","0LLRRL","0LLLR","0LLLL","0LRR"};
     boolean [] endings  = {false,false,false,false,false,false}; 
-    {
+    System.out.println("Controls are L, R, B or Back, V or View, G or Grab and Reset. Type 0 to start your adventure");
         String input = ""; 
         boolean validInput = false;// to check if the input is valid
         boolean gameStart = false;
@@ -66,24 +66,25 @@ public class Heist
         boolean hasCutter = false; 
         boolean hasBlaster = false; 
         boolean look = false; 
-    if(input.equals("")){
-                System.out.println("Controls are L, R, B or Back and Reset. Type 0 to start your adventure");
-    }
     while(gameRunning){
         String input2 = sc.next(); 
         input2 = input2.toUpperCase(); 
-        boolean end = false; 
-        if (input2.equals("0")&& gameStart == false&& input.equals("")){
+        boolean end = false;
+        if(!gameStart){
+        if (input2.equals("0")){
         gameStart = true;
-        input = input+input2;
-        validInput = true;}
-        
+        input = input+input2; 
+        validInput = true;
+        continue;    
+        }else{
+        System.out.println("You've already started the game");
+        continue;
+        }}
         if(input2.equals("STOP")){
         System.out.println("Thanks for playing");
         gameRunning = false;
-        break; }
-                
-        if(!gameStart && !input2.equals("0")){
+        break;}  
+        if(!gameStart){
             System.out.println("Please enter 0 to start the game");
         }
        
@@ -93,17 +94,17 @@ public class Heist
             System.out.println("You have reset, press 0 to start");
             gameStart = false; 
             }  
-            boolean foundView =false;
+            boolean foundView = false;
         if(input2.equals("VIEW")||input2.equals("V")){
             validInput = true;  
         for(int y  = 0; y < view.length; y++){
           if(input.equals(view[y])){
-            System.out.println(viewID[y]);
+            System.out.println(viewID[y] + "\n" );
             foundView = true; 
             look = true; 
         }
         }
-        } else if(foundView = false){
+        } else if(foundView == false){
         System.out.println("You can't see anything");
         } 
         if(input2.equals("GRAB")||input2.equals("G")){
@@ -121,7 +122,7 @@ public class Heist
                 roomData[8] = "You sneak into the hallway and to your luck no guards are around and find yourself \n standing underneath a huge glass box suspended in the air holding the precious diamond, now \n you have two options, Use item the sonic blaster to blast open the case or see if you can scale up to the diamond and \n climb into the box.";
                 validInput = true; 
         } else {
-        System.out.println("There's nothing to gab here"); 
+        System.out.println("There's nothing to grab here"); 
         }
         }}
         if((input2.equals("BACK")||input2.equals("B"))&& gameStart == true){
@@ -132,22 +133,22 @@ public class Heist
             }
             validInput = true; 
         }
-        if(input.equals("0RRR") && hasBlaster == false && input2.equals("R")){
+        if(input.equals("0RRR") && !hasBlaster && input2.equals("R")){
         validInput = false; 
         System.out.println("What on earth are you doing");
         continue; 
-        }else if(input.equals("0LLRR")&& hasCutter == false && input.equals("R")){
+        }else if(input.equals("0LLRR")&& !hasCutter && input.equals("R")){
         validInput = false; 
         System.out.println("You have no item that will help you");
         continue; 
         }
-        else if(gameStart == true && (input2.equals("R")||input2.equals("L"))){
+        else if(gameStart&& (input2.equals("R")||input2.equals("L"))){
             look = false; 
             input  = input+input2;      
             validInput = true; 
             } 
             
-        if(gameStart == false){
+        if(!gameStart){
             System.out.println("this is not a valid input please input 0 to start");  
             }
             
@@ -155,7 +156,7 @@ public class Heist
             boolean inRoom = false; 
             for(int i = 0; i<rooms.length; i++){
                 if(rooms[i].equals(input)){
-                    System.out.println(roomData[i]);
+                    System.out.println(roomData[i] + "\n");
                     inRoom = true;
                     break; 
                 }
@@ -169,7 +170,7 @@ public class Heist
                 if(endingsID[j].equals(input)){
                 endings[j] = true; 
                 end = true;
-                }}}
+                }}
         if (end){
             input = "";
             System.out.println("You have finished the adventure and have been reset. Press 0 to start again or type 'stop' to close the game");
@@ -180,10 +181,10 @@ public class Heist
         for(int x = 0; x<endings.length; x++){
             endings[x] = false; 
                 }
-                } 
-                }
-                }
-                                    
+                } }
+            }
+            
+                                
         
     /**
      * This method checks if all endings have been triggered 
